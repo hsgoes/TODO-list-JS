63ef2c29b4836934b17366f1fb7ef2240c087ef0
Hugo Goes <hugo.henkel@hotmail.com>
2020-02-06 16:54:37 -0300

    Projeto Finalizado

--- js/script.js
+++ js/script.js
@@ -1,18 +1,44 @@
 let botaoAdicionar = document.getElementById("adicionar-tarefa")
 let tarefas = document.getElementById("tasks")
 let inputTarefa = document.getElementById("tarefa-digitada")
+let listaTarefas = localStorage.getItem('listaDeTarefas') ? JSON.parse(localStorage.getItem('listaDeTarefas')) : []
 
-let novaTarefa = `<div class="col-md-4">
-    <div class="cardTarefa row">
-        <div class="texto-tarefa">
-
-        </div>
-        <div class="botao-tarefa">
-            <img src="imagens/check.png" width="32">
-        </div>
+const mostrarNaTela = arrayTarefas => {
+    arrayTarefas.forEach(textoTarefa => {
+        let novaTarefa = `<div class="col-md-4">
+    <div class="cardTarefa">
+            <div class="texto-tarefa overflow-auto" style="margin-left: 10px">
+                ${textoTarefa}
+            </div>
+            <div class="botao-tarefa">
+                <img src="imagens/check.png" width="32">
+            </div>
     </div>
-</div>`
+    </div>`
 
+        //Insere uma nova tarefa após "beforeend" depois da tag pai das tarefas
+        tarefas.insertAdjacentHTML("beforeend", novaTarefa)
+
+        let objTarefaNova = tarefas.lastElementChild
+
+        btnCheckTarefaNova = objTarefaNova.lastElementChild.lastElementChild
+
+        btnCheckTarefaNova.onclick = (event) => {
+            event.target.parentNode.parentNode.parentNode.remove()
+            listaTarefas = listaTarefas.filter(valor => valor != textoTarefa)
+
+            salvarTarefaLocalStorage(listaTarefas)
+        }
+    });
+}
+
+mostrarNaTela(listaTarefas);
+
+const salvarTarefaLocalStorage = tarefas => {
+    let tarefasJSON = JSON.stringify(tarefas)
+    localStorage.setItem("listaDeTarefas", tarefasJSON)
+    console.log("Lista de tarefas salva!")
+}
 
 
 const criarTarefa = (event) => {
@@ -24,6 +50,10 @@ const criarTarefa = (event) => {
             alert("Você deve digitar uma tarefa")
             return
         }
+
+        listaTarefas.push(valorDigitado)
+        salvarTarefaLocalStorage(listaTarefas)
+
         //limpa o campo
         inputTarefa.value = "";
 
@@ -40,13 +70,16 @@ const criarTarefa = (event) => {
 
         //Insere uma nova tarefa após "beforeend" depois da tag pai das tarefas
         tarefas.insertAdjacentHTML("beforeend", novaTarefa)
-        
+
         let objTarefaNova = tarefas.lastElementChild
 
         btnCheckTarefaNova = objTarefaNova.lastElementChild.lastElementChild
 
-        btnCheckTarefaNova.onclick = (event)=> {
+        btnCheckTarefaNova.onclick = (event) => {
             event.target.parentNode.parentNode.parentNode.remove()
+            listaTarefas = listaTarefas.filter(valor => valor != valorDigitado)
+
+            salvarTarefaLocalStorage(listaTarefas)
         }
     }
 }
@@ -55,4 +88,6 @@ const criarTarefa = (event) => {
 inputTarefa.addEventListener("keypress", criarTarefa)
 botaoAdicionar.addEventListener("click", criarTarefa)
 
+const construirTarefas = () => {
 
+}
